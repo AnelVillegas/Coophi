6cff7d96a65f7a1e6e2e1388127b6638d4dd5bd7
anel <hanhelle@gmail.com>
2019-08-21 13:42:18 -0500

    Updates

---
+++ precache-manifest.778014fc2e548d258ec81ab396311c65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d750239de05728acd9464eb38bdbdf9",
+    "url": "/Coophi/index.html"
+  },
+  {
+    "revision": "fda3e1b605ef60cd356c",
+    "url": "/Coophi/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "364de0bdd9047745e83d",
+    "url": "/Coophi/static/js/2.640f993e.chunk.js"
+  },
+  {
+    "revision": "fda3e1b605ef60cd356c",
+    "url": "/Coophi/static/js/main.6d28d998.chunk.js"
+  },
+  {
+    "revision": "068427c4e1d930328827",
+    "url": "/Coophi/static/js/runtime~main.00e853a2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Coophi/static/media/logo.5d5d9eef.svg"
+  }
+]);
